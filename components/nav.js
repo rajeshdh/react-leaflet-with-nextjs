e04c5c848d30d81a6f2ebdaeb3908edb4c5b7948
rajesh dhiman <rajeshdhimanskt@gmail.com>
2019-02-26 22:03:46 +0530

    adds github link

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
+  { href: 'https://github.com/rajeshdh/react-leaflet-with-nextjs', label: 'Github' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
